697053020cb14a81058970e0d33b5cd9f3d91cd1
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:20:54 +0300

    Update index.php

--- usein_kysytyt_kysymykset/index.php
+++ usein_kysytyt_kysymykset/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="fi">
-
+ 
 <head>
 
   <script>
@@ -11,26 +11,32 @@
 
   </script>
 
-    <title>Yhteistyössä - Jaetaan.net</title>
+
+    <title>Usein kysytyt kysymykset - Jaetaan.net</title>
 
     <link rel='stylesheet' type='text/css' href='../muotoilu.css'>
 
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <meta charset='UTF-8'>
-    <meta name='keywords' content='yhteistyössä, yhteistyökumppanit, Jaetaan.net'>
-    <meta name='description' content='Tähän tulee tekstiä myöhemmin.'>
+    <meta charset='UTF-8'> 
+    <meta name='keywords' content='ukk, usein kysytyt kysymykset, yksityiskohtaisempaa tietoa, Jaetaan.net'>
+    <meta name='description' content='Yksityiskohtaisempaa tietoa Jaetaan.netistä. 
+                                      Jos et löydä kysymyksiisi vastausta 
+                                      muuten, kysy yhteyssivun kautta 
+                                      suoraan ylläpitäjiltä. Vastaamme mielellämme.'>
 
-    <meta property="og:title" content="Yhteistyössä" />
-    <meta property="og:description" content="Tähän tulee tekstiä myöhemmin." />
+    <meta property="og:title" content="Usein kysytyt kysymykset" />
+    <meta property="og:description" content="Yksityiskohtaisempaa tietoa Jaetaan.netistä. Jos et löydä 
+                                             haluamaasi tietoa muutoin, kysy yhteyssivun kautta suoraan 
+                                             meiltä. Vastaamme mielellämme." />
     <meta property="og:image" content="http://jaetaan.net/kuvat/kaivopuisto-helsinki.jpg" />
-    <meta property="og:url" content="http://jaetaan.net/yhteistyossa/" />
+    <meta property="og:url" content="http://jaetaan.net/usein_kysytyt_kysymykset/" />
     <meta property="og:type" content="website" /> 
     <meta property="fb:app_id" content="your fb app id here" /> 
     <meta property="og:site_name" content="Jaetaan.net" /> 
     <meta property="og:locale" content="fi_FI">
 
     <script src='../toiminnallisuuksia.js'></script>
-
+ 
 
 </head>
 
@@ -41,7 +47,7 @@
         <?php 
             include '../includes/header.php';
             include '../includes/nav_bar_public.php';
-            include '../includes/yhteistyossa.php';
+            include '../includes/ukk.php';
             include '../includes/footer.php';
         ?>
 
