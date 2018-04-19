de163dd8d555c5369e85787949bad5fc80dc704f
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:40:21 +0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,96 @@
+<!-- Footer pienelle näytölle. Katoaa isolla näytöllä. -->
+
+<div id='footer_pieni_naytto'>
+
+    <ul>
+ 
+        <a href='https://jaetaan.net/info'>
+            
+            <li class='footer_pieni_naytto_lista' >
+                Info
+            </li>
+
+        </a>
+
+        <a href='https://jaetaan.net/usein_kysytyt_kysymykset'>
+             
+            <li class='footer_pieni_naytto_lista'>
+                UKK
+            </li>
+
+        </a>
+
+        <a href='https://jaetaan.net/turvallisuustietoa'>
+            
+            <li class='footer_pieni_naytto_lista'>
+                Turvallisuustietoa
+            </li>
+                
+        </a>
+
+        <a href='https://jaetaan.net/yhteistyossa'>
+            
+            <li class='footer_pieni_naytto_lista'>
+                Yhteistyössä
+            </li>
+
+        </a>
+
+        <a href='https://jaetaan.net/yhteys'>
+                
+            <li class='footer_pieni_naytto_lista'>
+                Yhteys
+            </li>
+              
+        </a>
+
+    </ul>
+
+</div>
+
+
+<!-- Footer isolle näytölle. Katoaa pienellä näytöllä. -->
+
+<div id='footer_iso_naytto'>
+
+    <p class='footer_iso_naytto_kappale'>
+            
+        <a class='footer_linkki' href='https://jaetaan.net/info'>
+            Info
+        </a>
+            
+    </p>
+
+    <p class='footer_iso_naytto_kappale'>
+        
+        <a class='footer_linkki' href='https://jaetaan.net/usein_kysytyt_kysymykset'>
+            UKK
+        </a>
+
+    </p>
+
+    <p class='footer_iso_naytto_kappale'>
+            
+        <a class='footer_linkki' href='https://jaetaan.net/turvallisuustietoa'>
+            Turvallisuustietoa    
+        </a> 
+            
+    </p>
+
+    <p class='footer_iso_naytto_kappale'>
+            
+        <a class='footer_linkki' href='https://jaetaan.net/yhteistyossa'>
+            Yhteistyössä
+        </a>
+            
+    </p>
+
+    <p class='footer_iso_naytto_kappale'>
+        
+        <a class='footer_linkki' href='https://jaetaan.net/yhteys'>
+            Yhteys
+        </a>
+    
+    </p>
+
+</div>
