469215d463a04afc6a68a126c4855764870f2d8e
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:18:04 +0300

    Create index.php

---
+++ turvallisuustietoa/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fi">
+
+<head>
+
+  <script>
+
+    if(window.location.protocol != 'https:') {
+      location.href = location.href.replace("http://", "https://");
+    }
+
+  </script>
+
+    <title>Turvallisuustietoa - Jaetaan.net</title>
+
+    <link rel='stylesheet' type='text/css' href='../muotoilu.css'>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta charset='UTF-8'>
+    <meta name='keywords' content='sivuston turvallinen käyttäminen, yksityisyyden suoja, Jaetaan.net'>
+    <meta name='description' content='Tietoa Jaetaan.netin turvallisesta käyttämisestä ja siitä, 
+                                      kuinka turvaamme yksityisyytesi.'>
+
+    <meta property="og:title" content="Turvallisuustietoa" />
+    <meta property="og:description" content="Tietoa Jaetaan.netin turvallisesta käyttämisestä ja siitä, 
+                                             kuinka turvaamme yksityisyytesi." />
+    <meta property="og:image" content="http://jaetaan.net/kuvat/kaivopuisto-helsinki.jpg" />
+    <meta property="og:url" content="http://jaetaan.net/turvallisuustietoa/" />
+    <meta property="og:type" content="website" /> 
+    <meta property="fb:app_id" content="your fb abb id here" /> 
+    <meta property="og:site_name" content="Jaetaan.net" /> 
+    <meta property="og:locale" content="fi_FI">
+
+     <script src='../toiminnallisuuksia.js'></script>
+
+
+</head>
+
+
+<body>
+
+
+        <?php 
+            include '../includes/header.php';
+            include '../includes/nav_bar_public.php';
+            include '../includes/turvallisuustietoa.php';
+            include '../includes/footer.php';
+
+        ?>
+
+
+</body>
+</html>
