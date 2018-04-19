a1da0ffa7b69428cc399c8c9d643744721a9fe03
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:47:28 +0300

    Create turvallisuustietoa.php

---
+++ includes/turvallisuustietoa.php
@@ -0,0 +1,29 @@
+    <div class='keski_osa'>
+
+        <h1 class='info_otsikko'>
+    	    Turvallisuustietoa
+        </h1>
+
+        <p class='info_teksti'>
+            Jaetaan.net toimii ainoastaan ihmisten välisen 
+            yhteistyön välittäjänä ja sen käyttäminen on kaikille avointa. 
+            Jaetaan.net ei takaa palvelun käyttäjien 
+            luotettavuutta. Jaetaan.net ei vastaa 
+            palvelun käyttäjien välisistä mahdollisista erimielisyyksistä. 
+            Erimielisyyksien sattuessa osapuolet sopivat niistä keskenään, 
+            viime kädessä Suomen lain ja oikeusistuimien avustamana.
+
+        <br>
+        <br>
+
+            Jaetaan.net ei kerää käyttäjistään minkäänlaisia tietoja. Kun käyttäjä 
+            poistaa lisäämänsä ilmoituksen tai kun se poistuu automaattisesti, 
+            kaikki kyseiseen ilmoitukseen liittyvät tiedot ja kuvat poistuvat 
+            Jaetaan.netin järjestelmistä pysyvästi.
+
+        <br>
+        <br>
+
+        </p>
+
+    </div>
