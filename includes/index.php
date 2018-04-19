9af927a9218650a5c7e3c417e88bb5b3d9c5bc08
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 22:07:03 +0300

    Create index.php

---
+++ includes/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fi">
+
+<head>
+
+  <script>
+
+    if(window.location.protocol != 'https:') {
+      location.href = location.href.replace("http://", "https://");
+    }
+
+  </script>
+
+  <title>Jaetaan.net</title>
+
+</head>
+
+
+<body>
+
+</body>
+</html>
