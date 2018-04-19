8149a76dd673f56934b07ebc2ffa3f04ed41289c
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:42:50 +0300

    Create ilmoitusten_haku_2.php

---
+++ includes/ilmoitusten_haku_2.php
@@ -0,0 +1,17 @@
+<div> 
+
+	<div>
+
+    <form action='https://jaetaan.net/admin/published.php?#listaus_ankkuri' method='get' enctype='multipart/form-data'>
+
+<span id='listaus_ankkuri'></span>
+
+            <input id='haku_sana_haku_2' type='search' name='sanahaku' maxlength='255' placeholder='Sanahaku' value="<?php if (isset($_GET['sanahaku'])) echo $_GET['sanahaku'];?>">
+            
+            <input id='hae_ilmoituksia_nappi_2' type='submit' value='Hae'>
+
+        </div>
+
+    </form>
+
+</div> 
