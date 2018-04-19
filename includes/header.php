fcd03302688bc19ba6612abc57275b41a088193c
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:41:05 +0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,5 @@
+<a href='https://jaetaan.net'><img id='header_logo_I' src='https://jaetaan.net/kuvat/jaetaan.net_header_logo_I.svg' alt='Jaetaan.netin logo'></a>
+
+<a href='https://jaetaan.net'><img id='header_logo_II' src='https://jaetaan.net/kuvat/jaetaan.net_header_logo_II.svg' alt='Jaetaan.netin logo'></a>
+
+<a href='https://jaetaan.net'><img id='header_logo_III' src='https://jaetaan.net/kuvat/jaetaan.net_header_logo_III.svg' alt='Jaetaan.netin logo'></a>
