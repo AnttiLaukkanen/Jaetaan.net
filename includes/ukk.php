4d76dda0cf10b5a0100276f93569362bdd4d458e
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:48:33 +0300

    Create ukk.php

---
+++ includes/ukk.php
@@ -0,0 +1,256 @@
+<div class='keski_osa'>
+
+      <noscript>
+          <br>
+          <p class = 'noscript_p'>Selaimessasi ei ole JavaScript päällä.</p>
+          <br>
+          <p>Tämä sivu toimii vain, jos JavaScript on päällä.</p>
+          <p>Laita JavaScript päälle ja lataa tämä sivu uudelleen.</p>
+      </noscript>
+
+    <div class = 'noscript'>
+
+      <script>
+          document.getElementsByClassName('noscript')[0].style.display='block';
+      </script>
+
+    <h1 class='info_otsikko'>UKK – usein kysytyt kysymykset</h1>
+
+    <p class='info_teksti_2'>Klikkaa kysymystä nähdäksesi vastauksen.</p>    
+    
+    <p class='info_teksti_2'>Klikkaa uudelleen piilottaaksesi vastauksen.</p>
+
+    <br>
+    <br>
+
+    <p id='ukk_kysymys_1' class='info_teksti' onclick='ukk_vastaus_nayta_piiloon_1()'>
+        <b>Voitteko antaa vinkkejä minkälainen on hyvä ilmoitus Jaetaan.netissä?</b></p>
+
+    <br>
+
+    <div id='ukk_vastaus_1'>
+
+        <p class='info_teksti'>Otsikointi ja kuvat ovat tärkeitä.</p>
+        <p class='info_teksti'>Etusivulla ilmoituksia selaava ihminen klikkaa jotain 
+            tiettyä ilmoitusta todennäköisesti, koska otsikossa tai kuvassa oli jotain 
+            häntä kiinnostavaa.</p>
+
+        <br>
+
+        <p class='info_teksti'>Otsikossa kannattaa välttää käyttämästä sanaa ilmainen ja samaa 
+            tarkoittavia muita ilmaisuja.</p>
+        <p class='info_teksti'>Otsikossa kannattaa välttää myös mainitsemasta kaupunkia, 
+            aikaa tai paikkaa.</p>
+
+        <br>
+
+        <p class='info_teksti'>Jaetaan.netissä on ilmoituksia pelkästään ilmaisista asioista.</p>
+        <p class='info_teksti'>Kaupunki, ja jos ilmoitettu, myös aika ja paikka näkyvät 
+            etusivun listauksessa muutenkin.</p>
+
+        <br>
+
+        <p class='info_teksti'>Otsikosta näkyy rajallinen määrä merkkejä etusivun listauksessa.</p>
+
+        <br>
+
+        <p class='info_teksti'>Siksi yllä mainittujen sijaan otsikossa kannattaa kuvata ilmoitettavaa asiaa 
+           muilta osin kiinnostavasti, kuvaavasti, lyhyesti ja ytimekkäästi.</p>
+
+        <br>
+
+        <p class='info_teksti'>Jos otsikon on tarpeellista olla pitkä, kirjoita kiinnostavimmat asiat 
+            otsikon alkuun. Näin ne näkyvät etusivun listauksessa ja suurimmalla todennäköisyydellä 
+            herättävät ilmoituksia selaavan kiinnostuksen.</p>
+
+        <br>
+
+        <p class='info_teksti'>Suosittelemme lämpimästi vähintään yhden valokuvan käyttämistä.</p>
+        <p class='info_teksti'>Kuvallinen ilmoitus herättää enemmän huomiota kuin kuvaton.</p>
+
+        <br>
+
+        <p class='info_teksti'>Jos sopivaa kuvaa ilmoitettavasta asiasta ei löydy, jokin 
+            yleisluontoinen kiva kuvakin saattaa toimia paremmin kuin täysin kuvaton ilmoitus.</p>
+
+        <br>
+
+        <p class='info_teksti'>Ja kuitenkin, jos minkäänmoista kuvaa ei löydy, ei hätää. 
+            Kiinnostavasti muotoiltu otsikko toimii hyvin myös itsenäisesti.</p>
+        <br>
+        <p class='info_teksti'>Ilmoituksen sisältöosuudessa kaikki oleellinen ja kiinnostava 
+            kannattaa kertoa alussa. Ilmoitettavaa asiaa voi tässä kuvailla pidemminkin. 
+            Kannattaa kuitenkin miettiä, kuinka pitkiä kuvauksia ihmiset jaksavat lukea loppuun asti.</p>
+        <br>
+        <p class='info_teksti'>Muilta osin Lisää ilmoitus -lomake opastaa sinua tekemään 
+            ilmoituksen, joka toimii hyvin Jaetaan.netissä.</p>
+        <br>
+
+    </div>
+
+    <p id='ukk_kysymys_2' class='info_teksti' onclick='ukk_vastaus_nayta_piiloon_2()'>
+    	<b>Järjestän live-musiikki tapahtuman, johon on vapaa pääsy. Tapahtumassa on 
+    	    kuitenkin myös myytiä; mm. mehua ja leivonnaisia sekä käsitöitä. Voinko 
+    	    ilmoittaa tapahtumasta Jaetaan.netissä?</b></p>
+
+    <br>
+
+    <div id='ukk_vastaus_2'>
+
+        <p class='info_teksti'>Kyllä voit, koska tapahtumassa on tarjolla jotain 
+        	ilmaiseksi – eli tässä tapauksessa live-musiikkia.</p> 
+
+        <p class='info_teksti'>Jaetaan.netissä ilmoittamisen kannalta ei ole väliä, vaikka 
+            samassa tapahtumassa on myös maksullista toimintaa.</p>
+
+        <br>
+       
+    </div>
+
+    <p id='ukk_kysymys_3' class='info_teksti' onclick='ukk_vastaus_nayta_piiloon_3()'><b>Järjestän tapahtuman, johon ei ole sisäänpääsymaksua. 
+        Tapahtumassa on kuitenkin eteispalvelumaksu / narikkamaksu, joka on kaikille pakollinen. 
+        Voinko ilmoittaa tapahtumasta Jaetaan.netissä?</b></p>
+
+    <br>
+
+    <div id='ukk_vastaus_3'>
+
+        <p class='info_teksti'>Et voi.</p> 
+
+        <p class='info_teksti'>Jaetaan.netissä voi ilmoittaa vain sellaisista tapahtumista, 
+            joihin ei ole pääsymaksua muodossa eikä toisessa.</p>
+
+        <p class='info_teksti'>Jos tapahtumassa on pakollinen narikka, sen tulee olla ilmainen. Jos taas 
+    	    tapahtumassa on maksullinen narikka, sen tulee olla vapaaehtoinen.</p>
+
+        <br>
+
+    </div>
+
+        <p id='ukk_kysymys_5' class='info_teksti' onclick='ukk_vastaus_nayta_piiloon_5()'>
+        <b>Mitä tarkoittaa, kun ilmoituksen ilmoittajana on Jaetaan.net? 
+            Entä Jaetaan.net automaatio?</b></p>
+
+    <br>
+
+        <div id='ukk_vastaus_5'>
+
+        <p class='info_teksti'>Kun ilmoittajana on Jaetaan.net, tarkoittaa se sitä, että 
+            joku Jaetaan.netin sisällön ylläpitäjistä on löytänyt tai saanut vinkin 
+            sellaisesta julkisesta ilmaisesta asiasta, mikä hän arvelee kiinnostavan ihmisiä.</p> 
+        <p class='info_teksti'>Ja hän on lisännyt tuon ilmoituksen sivustolle käsityönä.</p>
+
+        <br>
+
+        <p class='info_teksti'>Kun ilmoittajana on Jaetaan.net automaatio, tarkoittaa se sitä, 
+            että ilmoitus on haettu <a class='info_linkki_2' target='_blank' href='https://api.hel.fi/linkedevents/v1/event/'>
+            Helsingin kaupungin linked events -järjestelmästä</a> automaattisesti.</p>
+
+        <br>
+
+        <p class='info_teksti'>Linked events on Helsingin kaupungin ylläpitämä järjestelmä, johon 
+            tällä hetkellä on koottu ennen kaikkea Helsingin kaupungin kulttuuritoimen, Visit Helsingin
+            ja Helmet-kirjastojen tapahtumia.</p>
+    
+        <p class='info_teksti'>Lisäksi järjestelmästä löytyy jonkin verran Helsingin kaupungin 
+            muiden virastojen ja laitosten järjestämiä tapahtumia.</p>
+
+        <p class='info_teksti'>Linked events -järjestelmästä löytyy myös joitain 
+            Helsingin kaupungin ja Helmet-kirjastojen ulkopuolisten julkisten ja yksityisten 
+            tahojen järjestämiä tapahtumia.</p>
+
+        <br>
+
+        <p class='info_teksti'>Linked events -järjestelmästä voi kuka tahansa hakea 
+            ilmoituksia vapaasti ja ilmaiseksi.</p>
+
+        <br>
+
+    </div>
+
+    <p id='ukk_kysymys_6' class='info_teksti' onclick='ukk_vastaus_nayta_piiloon_6()'>
+    	<b>Kuka tai ketkä ovat Jaetaan.netin takana?</b></p>
+
+    <br>
+
+    <div id='ukk_vastaus_6'>
+
+        <p class='info_teksti'>Jaetaan.netin takana on Jaetaan-yhteisö.</p>
+
+        <p class='info_teksti'>Jaetaan-yhteisö on vapaamuotoinen eli se ei ole
+            yhdistys, järjestö, osuuskunta, yritys eikä mikään muukaan viralliseen 
+            muotoon rekisteröity joukko ihmisiä.</p>
+
+        <p class='info_teksti'>Jaetaan-yhteisöön itsensä kuuluvaksi määritteleviä 
+            ihmisiä yhdistää halu tavalla tai toisella edistää sellaista ihmisten 
+            välistä toimintaa, johon ei liity lainkaan raha eikä vaihtaminen.</p>
+
+        <br>
+
+        <p class='info_teksti'>Lisätietoa aiheesta saat <a href='https://www.facebook.com/groups/jaetaan' class='info_linkki_2' target='_blank'>
+            Jaetaan-yhteisön Facebook-ryhmästä</a>.
+        </p>
+
+        <br>
+
+        <p class='info_teksti'><a class='info_linkki_2' href='https://www.facebook.com/jaetaan.yllapito' target="_blank">
+            Antti Laukkanen</a> laittoi Jaetaan.netin alkuun.</p>
+        <p class='info_teksti'>Hän on kirjoittanut lähes koko sivuston koodin.</p>
+        <p class='info_teksti'>Antti on myös ylläpitänyt sivuston tekniikkaa ja sisältöä.</p>
+
+        <br>
+
+        <p class='info_teksti'><a class='info_linkki_2' href='https://www.facebook.com/markus.h.heikkila' target="_blank">
+            Markus Heikkilä</a> teki sivustolle logon ja ison osan 
+            muutakin ulkoasua.</p>
+
+        <br>
+
+        <p class='info_teksti'><a class='info_linkki_2' href='https://www.facebook.com/theqmage' target="_blank">
+            Marko Koskinen</a> ja 
+            <a class='info_linkki_2' href='https://juuso.vilmunen.net/' target="_blank">Juuso Vilmunen</a> ovat toimineet 
+            teknisinä neuvonantajina koodin kirjoittamisessa.</p>
+
+        <br>
+
+        <p class='info_teksti'>Sivuston käytettävyyttä, ulkoasua, ideaa 
+        	ja henkeä ovat yllämainittujen lisäksi pohdinnoillaan ja 
+        	kommenteillaan edistäneet useat muut yksittäiset ihmiset.</p>
+
+        <br>
+
+        <p class='info_teksti'>Jaetaan.net on kaikille avoin käyttää, kehittää ja edistää.</p> 
+
+        <p class='info_teksti'>Jos sinulla on ideoita ja halua tulla mukaan toteuttamaan 
+            niitä, tervetuloa!</p>
+
+        <p class='info_teksti'>Saat meihin yhteyden <a class='info_linkki_2' href='https://jaetaan.net/yhteys'>
+            yhteyssivulta löytyvällä yhteydenottolomakkeella</a>.</p>
+
+        <br>
+       
+    </div>
+
+    <p id='ukk_kysymys_7' class='info_teksti' onclick='ukk_vastaus_nayta_piiloon_7()'>
+    	<b>Onko Jaetaan.net avointa lähdekoodia?</b></p>
+
+    <br>
+
+        <div id='ukk_vastaus_7'>
+
+        <p class='info_teksti'>Kyllä on.</p>
+
+        <p class='info_teksti'>Koodi on kirjoitettu HTML, CSS, JavaScript, PHP 
+            ja SQL -kieliä käyttäen.</p>
+
+        <p class='info_teksti'>Jos haluat koodin itsellesi, ota meihin yhteyttä 
+            <a class='info_linkki_2' href='https://jaetaan.net/yhteys'>
+            yhteyssivulta löytyvällä yhteydenottolomakkeella</a>.</p>
+
+        <br>
+
+    </div>
+
+  </div>
+
+</div>
