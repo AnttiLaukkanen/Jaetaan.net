6b8c762e221e0c6213d508f002bf711db2288547
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:19:23 +0300

    Create index.php

---
+++ usein_kysytyt_kysymykset/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fi">
+
+<head>
+
+  <script>
+
+    if(window.location.protocol != 'https:') {
+      location.href = location.href.replace("http://", "https://");
+    }
+
+  </script>
+
+    <title>Yhteistyössä - Jaetaan.net</title>
+
+    <link rel='stylesheet' type='text/css' href='../muotoilu.css'>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta charset='UTF-8'>
+    <meta name='keywords' content='yhteistyössä, yhteistyökumppanit, Jaetaan.net'>
+    <meta name='description' content='Tähän tulee tekstiä myöhemmin.'>
+
+    <meta property="og:title" content="Yhteistyössä" />
+    <meta property="og:description" content="Tähän tulee tekstiä myöhemmin." />
+    <meta property="og:image" content="http://jaetaan.net/kuvat/kaivopuisto-helsinki.jpg" />
+    <meta property="og:url" content="http://jaetaan.net/yhteistyossa/" />
+    <meta property="og:type" content="website" /> 
+    <meta property="fb:app_id" content="your fb app id here" /> 
+    <meta property="og:site_name" content="Jaetaan.net" /> 
+    <meta property="og:locale" content="fi_FI">
+
+    <script src='../toiminnallisuuksia.js'></script>
+
+
+</head>
+
+
+<body>
+
+
+        <?php 
+            include '../includes/header.php';
+            include '../includes/nav_bar_public.php';
+            include '../includes/yhteistyossa.php';
+            include '../includes/footer.php';
+        ?>
+
+
+</body>
+</html>
