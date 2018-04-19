8bb6fdaa7a75c3811b39e5b09ad514622221f224
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:16:39 +0300

    Create index.php

---
+++ info/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fi">
+
+<head>
+
+  <script>
+
+    if(window.location.protocol != 'https:') {
+      location.href = location.href.replace("http://", "https://");
+    }
+
+  </script>
+
+
+    <title>Info - Jaetaan.net</title>
+
+    <link rel='stylesheet' type='text/css' href='../muotoilu.css'>
+
+    <link rel="shortcut icon" type="image/png" href="http://jaetaan.net/kuvat/jaetaan_net_favicon.png"/>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta charset='UTF-8'>
+    <meta name='keywords' content='info, tietoa, yleistä tietoa, Jaetaan.net'>
+    <meta name='description' content='Yleistä tietoa Jaetaan.netistä. 
+                                      Jos et löydä kysymyksiisi vastausta 
+                                      muuten, kysy yhteyssivun kautta 
+                                      suoraan ylläpitäjiltä. Vastaamme mielellämme.'>
+
+    <meta property="og:title" content="Info" />
+    <meta property="og:description" content="Yleistä tietoa Jaetaan.netistä. 
+                                             Jos et löydä kysymyksiisi vastausta 
+                                             muuten, kysy yhteyssivun kautta 
+                                             suoraan ylläpitäjiltä. Vastaamme mielellämme." />
+    <meta property="og:image" content="http://jaetaan.net/kuvat/kaivopuisto-helsinki.jpg" />
+    <meta property="og:url" content="http://jaetaan.net/info/" />
+    <meta property="og:type" content="website" /> 
+    <meta property="fb:app_id" content="your fb app id here" /> 
+    <meta property="og:site_name" content="Jaetaan.net" /> 
+    <meta property="og:locale" content="fi_FI">
+
+    <script src='../toiminnallisuuksia.js'></script>
+
+</head>
+
+
+<body>
+
+
+        <?php 
+            include '../includes/header.php';
+            include '../includes/nav_bar_public.php';
+            include '../includes/info.php';
+            include '../includes/footer.php';
+        ?>
+
+
+</body>
+</html>
