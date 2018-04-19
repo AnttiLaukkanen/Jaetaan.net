3c6622124d8532bc457853861b44f3c0b4f62074
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:46:29 +0300

    Create info.php

---
+++ includes/info.php
@@ -0,0 +1,92 @@
+<div class='keski_osa'>
+
+      <noscript>
+          <br>
+          <p class = 'noscript_p'>Selaimessasi ei ole JavaScript päällä.</p>
+          <br>
+          <p>Tämä sivu toimii vain, jos JavaScript on päällä.</p>
+          <p>Laita JavaScript päälle ja lataa tämä sivu uudelleen.</p>
+      </noscript>
+
+    <div class = 'noscript'>
+ 
+      <script>
+      document.getElementsByClassName('noscript')[0].style.display='block';
+      </script>
+
+        <h1 class='info_otsikko'>
+        	Info
+        </h1>
+
+        <h3 class='info_ala_ala_otsikko'>
+            Jaetaan.net kokoaa tiedon pääkaupunkiseudun 
+            ilmaisista asioista yhteen paikkaan helposti löydettäväksi.
+        </h3> 
+
+        <h3 class='info_ala_ala_otsikko'>
+            Selaa ilmoituksia ja ilmoita itse!
+        </h3> 
+
+        <br>
+
+        <h3 class='info_ala_ala_otsikko'>
+        	Täällä voit ilmoittaa:
+        </h3> 
+        
+        <br>
+        
+        <ul class='info_lista'>
+
+            <li style='list-style-type:disc;'>tavaran antamisesta ja pyytämisestä</li>  
+            
+            <li style='list-style-type:disc;'>tavaran lainaamisesta</li> 
+            <li style='list-style-type:disc;'>avun antamisesta ja pyytämisestä</li>
+            <li style='list-style-type:disc;'>tapahtumasta</li> 
+            <li style='list-style-type:disc;'>luennosta</li> 
+            <li style='list-style-type:disc;'>jne.</li>
+        
+        </ul>
+
+        <br>
+
+        <h3 class='info_ala_ala_otsikko'>
+        	Täällä et voi ilmoittaa:
+        </h3> 
+        
+        <br>
+ 
+        <ul class='info_lista'>
+           <li style='list-style-type:disc;'>myymisestä</li> 
+           <li style='list-style-type:disc;'>ostamisesta</li> 
+           <li style='list-style-type:disc;'>vuokraamisesta</li> 
+           <li style='list-style-type:disc;'>vaihtamisesta</li> 
+           <li style='list-style-type:disc;'>jne.</li>
+        </ul>
+
+        <br>
+    
+
+        <h3 class='info_ala_ala_otsikko'>
+	        Jaetaan.netin käyttäminen on kaikille ihmisille 
+         	ja yhteisöille avointa ja ilmaista.
+        </h3> 
+
+        <p class='info_teksti'>
+	        Yhteisöllä tarkoitamme yhdistyksiä, järjestöjä, kuntia, valtioita, yrityksiä, seurakuntia, 
+            urheiluseuroja, taloyhtiöitä, kaveriporukoita jne.
+        </p>
+
+        <br>
+
+        <p class='info_teksti'>
+            Lisätietoa Jaetaan.netistä löytyy kohdista 
+            <a class='info_linkki' href='https://jaetaan.net/usein_kysytyt_kysymykset'>UKK (usein kysytyt kysymykset)</a> 
+            ja <a class='info_linkki' href='https://jaetaan.net/turvallisuustietoa'>Turvallisuustietoa</a>.
+        </p>
+
+        <br>
+        <br>
+
+        </div>
+
+    </div>
