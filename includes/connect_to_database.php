22cf82f6b3a5efc7c7a284086bf058c7c355da00
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-19 18:33:00 +0300

    Create connect_to_database.php

---
+++ includes/connect_to_database.php
@@ -0,0 +1,15 @@
+        <?php
+
+            $servername = 'your server name';
+            $username = 'your user name';
+            $password = 'your password';
+            $dbname = 'your database name';
+
+            // Luo yhteyden
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            //Tarkistaa yhteyden
+            if (!$conn) {
+                die('Jokin meni pieleen. Yritä kohta uudestaan.');
+            }
+
+        ?>
