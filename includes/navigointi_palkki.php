096a04826ed6d782528d38d6203e12e5c3d126ee
AnttiLaukkanen <antti.laukkanen@gmail.com>
2018-04-20 21:37:25 +0300

    Create navigointi_palkki.php

---
+++ includes/navigointi_palkki.php
@@ -0,0 +1,19 @@
+<div id='navigointi'> 
+
+    <ul>
+            
+        <a id='etusivu_linkki' href='https://jaetaan.net'>
+            <li id='etusivu_lista'>
+                Etusivu
+            </li>
+        </a>
+
+        <a id='lisaa_ilmoitus_linkki' href='https://jaetaan.net/lisaa_ilmoitus'>
+            <li id='lisaa_ilmoitus_lista'>
+                Lisää ilmoitus
+            </li>
+        </a>
+
+    </ul>
+
+</div>
